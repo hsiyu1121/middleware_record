f01e3b5c25916b64d6edd5386bbbde5929f669c6
hsiyu1121 <bluefish1121@gmail.com>
2020-08-07 09:51:49 +0800

    feat: add server log time

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const moment = require("moment");
+
+
+app.use(function (req, res, next) {
+  console.log(`${moment().format('YYYY-MM-DD HH:mm:ss')} | ${req.method} from ${req.path}`)
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("列出全部 Todo");
+});
+
+app.get("/new", (req, res) => {
+  res.send("新增 Todo 頁面");
+});
+
+app.get("/:id", (req, res) => {
+  res.send("顯示一筆 Todo");
+});
+
+app.post("/", (req, res) => {
+  res.send("新增一筆  Todo");
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
